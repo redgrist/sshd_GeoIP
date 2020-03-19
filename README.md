90874ccfe67c176a999a30567eb7bca57cc938f5
Łukasz Komor <redgrist@gmail.com>
2020-03-19 07:44:39 +0100

    GeoIP for SSHD

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # sshd_GeoIP
 How to setup GeoIP to prevent access from other countries then PL 
+
+# install dnf, yum, apt
+sudo dnf install geoip-bin geoip-database
+# create file sh
+vim /usr/local/bin/ipfilter.sh
+# chmod
+chmod +x /usr/local/bin/ipfilter.sh
+# /etc/hosts.deny add:
+sshd: ALL
+# /etc/hosts.allow add:
+sshd: ALL: aclexec /usr/local/bin/ipfilter.sh %a
+# GeoIP fresh data download
+https://www.miyuru.lk/geoiplegacy
+# File ipv4: 
+https://dl.miyuru.lk/geoip/maxmind/country/maxmind4.dat.gz
+# gunzip and put file: 
+cd /usr/share/GeoIP/
+as GeoIP.dat
---
+++ ipfilter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# License: WTFPL
+
+# UPPERCASE space-separated country codes to ACCEPT example "PL US"
+ALLOW_COUNTRIES="PL"
+LOGDENY_FACILITY="authpriv.notice"
+
+if [ $# -ne 1 ]; then
+  echo "Usage:  `basename $0` " 1>&2
+  exit 0 # return true in case of config issue
+fi
+
+if [[ "`echo $1 | grep ':'`" != "" ]] ; then
+  COUNTRY=`/usr/bin/geoiplookup6 "$1" | awk -F ": " '{ print $2 }' | awk -F "," '{ print $1 }' | head -n 1`
+else
+  COUNTRY=`/usr/bin/geoiplookup "$1" | awk -F ": " '{ print $2 }' | awk -F "," '{ print $1 }' | head -n 1`
+fi
+[[ $COUNTRY = "IP Address not found" || $ALLOW_COUNTRIES =~ $COUNTRY ]] && RESPONSE="ALLOW" || RESPONSE="DENY"
+
+if [[ "$RESPONSE" == "ALLOW" ]] ; then
+  logger -p $LOGDENY_FACILITY "$RESPONSE sshd connection from $1 ($COUNTRY)"
+  exit 0
+else
+  logger -p $LOGDENY_FACILITY "$RESPONSE sshd connection from $1 ($COUNTRY)"
+  exit 1
+fi
